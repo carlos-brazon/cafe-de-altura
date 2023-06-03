9a51f2c223b5dd7f6c416294a6448c9004adbd43
carlos-brazon <carlosbrazon.sp@gmail.com>
2023-06-03 12:51:35 +0200

    commit de los radios del carrito

--- js/basket.js
+++ js/basket.js
@@ -1,47 +1,108 @@
 
+const counterBody = document.querySelector('.basket-inicial h2 span')
 const counterFloat = document.querySelector('.counter-float p');
+const counterBag = document.querySelectorAll('.basket-counter p')
 const divPrincipalBags = document.querySelector('.coffebag');
+const subtotal = document.querySelector('.subtotal span');
+const envio = document.querySelectorAll('input');
+console.log(envio[0]);
+const divenvio = document.querySelectorAll('.subtotal span')[1];
+console.log(divenvio);
+const h3envio= document.querySelectorAll('.basket-shipping')
 
 
-const deleteBag = (pais) => {
+envio[0].addEventListener('click', (event) =>{
+    console.log(event);
+    if (event) {
+        divenvio.innerHTML
+    }
+})
+envio[1].addEventListener('click', (event) =>{
+    console.log(event);
+    if (event) {
+        divenvio.innerHTML=h3envio[1].lastElementChild.innerHTML 
+    }
+})
+// const data = JSON.parse(localStorage.getItem("data")) || [];
+// data.forEach(e =>{
+//     subtotal.innerHTML=(e.precio.slice(0, -1).replace(',', '.'))
+//     console.log(subtotal.innerHTM);
+//     let tt= Number(subtotal.innerHTML)
+//     console.log(tt);
+//     tt+= Number(subtotal.innerHTML)
+//     console.log(tt);
+// })
+
+
+const addBag = (pais, cantidad) => {
     const data = JSON.parse(localStorage.getItem("data")) || [];
-    // const bagToDelete = data.find(bag => true);
-    const newBag = data.filter(bag => bag.pais !== pais);
-    localStorage.setItem("data", JSON.stringify(newBag));
-    console.log(newBag);
-    paintBags(newBag);
-    // // console.log(bag);
-    // console.log(bagToDelete);
-    // if (bagToDelete.quantity > 1) {
-        //     bagToDelete.quantity -= 1;
-        //     localStorage.setItem("data", JSON.stringify(data));
-        //     paintPokemons(data);
-        // } else {
-            //     const newBag = data.filter(bag => bag.pais !== pais);
-            //     localStorage.setItem("data", JSON.stringify(newBag));
-            //     paintBags(newBag);
-    // }
+    console.log(cantidad);
+    if (cantidad >= 1) {
+        data.forEach(e => {
+            if (e.pais === pais) {
+                e.quantity += 1
+                localStorage.setItem("data", JSON.stringify(data))
+                paintBags(data);
+            }
+        });
+    }
 }
 
+const deleteBag = (pais, cantidad) => {
+    const data = JSON.parse(localStorage.getItem("data")) || [];
+    if (cantidad > 1) {
+        data.forEach(e => {
+            if (e.pais === pais) {
+                e.quantity -= 1
+                localStorage.setItem("data", JSON.stringify(data))
+                paintBags(data);
+            }
+        });
+    }
+    else {
+        const newBag = data.filter(bag => bag.pais !== pais);
+        localStorage.setItem("data", JSON.stringify(newBag)); //no eliminar
+        paintBags(newBag);
+    }
+}
 
 const paintBags = (bags) => {
-    console.log(!bags.length);
-    // if (!bags.length) {
-        //     localStorage.setItem('data', JSON.stringify([]));
-        //     counterFloat.innerHTML = 0;
-        //     divPrincipalBags.innerText = "There is not Item";
-        // }
-        // else {
-            const bagss = JSON.parse(localStorage.getItem('data'));
-            counterFloat.innerHTML = bags.length;
-            bagss.forEach((e, i) => {
+    divPrincipalBags.innerHTML = '';
+    if (!bags.length) {
+        localStorage.setItem('data', JSON.stringify([]));
+        counterFloat.innerHTML = 0;
+        divPrincipalBags.innerText = "There is not Item";
+    }
+    else {
+
+        const bagss = JSON.parse(localStorage.getItem('data'));
+        counterFloat.innerHTML = bags.length;
+        counterBody.innerHTML = bags.length;
+
+    //   envio.forEach(e =>{
+    //       e.addEventListener('click', (event)=> {
+    //         console.log(event);
+    //           if (envio[0]) {
+    //               divenvio.innerHTML=h3envio[1].lastElementChild.innerHTML
+    //             } else{
+    //                 divenvio.innerHTML
+    //             }
+    //         })
+    //     })
+        let subtotalPaint = 0
+        bagss.forEach((e, i) => {
+            e.precio = ((e.quantity * Number(e.precio.slice(0, -1).replace(',', '.'))).toFixed(2)).toString().replace('.', ',').concat(' €');
+
+            subtotalPaint += (Number(e.precio.slice(0, -1).replace(',', '.')));
+            
+
             const bage = document.createElement("div");
-            bage.classList="product flex";
+            bage.classList = "product flex";
             bage.innerHTML = (`
                                     <div class="basket-counter flex">
-                                        <button class="basket-menos" onClick="deleteBag('${e.pais}')"></button>
-                                        <p class="flex">${i + 1}</p>
-                                        <div class="basket-mas"></div>
+                                        <div class="basket-menos" onClick="deleteBag('${e.pais}','${e.quantity}')"></div>
+                                        <p class="flex">${e.quantity}</p>
+                                        <div class="basket-mas" onClick="addBag('${e.pais}','${e.quantity}')"></div>
                                     </div>
                                     <div class="basket-coffebag">
                                         <div class="basket-bag" style="background-image: url(${e.url});"></div>
@@ -52,22 +113,29 @@ const paintBags = (bags) => {
                                     </div>
                                     <h3>${e.precio}</h3>
                                     `);
-            
+            counterBag.innerHTML = e.quantity
+
             divPrincipalBags.append(bage);
 
-        const divDivider= document.createElement('div');
-        divDivider.classList="basket-divider";
+            const divDivider = document.createElement('div');
+            divDivider.classList = "basket-divider";
             divPrincipalBags.append(divDivider);
         });
         divPrincipalBags.lastChild.remove();
-    // };
+        subtotal.innerHTML = subtotalPaint.toFixed(2).concat(' €');
+    };
 };
 
-if (true) {
-    paintBags(deleteBag);
+if (JSON.parse(localStorage.getItem('data'))) {
+    paintBags(JSON.parse(localStorage.getItem("data")));
 }
 else {
-    divPrincipalBags.innerText = "No items in cart!";
+    counterFloat.innerHTML = 0;
+    counterBody.innerHTML = 0;
+    divPrincipalBags.innerText = "There is not Item";
 }
+
+
+
 
 
--- js/store.js
+++ js/store.js
@@ -1,54 +1,92 @@
-const buttons = document.querySelectorAll(".imgn button:last-child");
-console.log(JSON.parse(localStorage.getItem("data"))==null);
+// const bags= [
+//     {pais:'Costa Rica Tarrazú', precio:9, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag.png"},
+//     {pais:'Colombia Los Naranjos', precio:9, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag2.png"},
+//     {pais:'Laos Amanecer', precio:9, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag3.png"},
+//     {pais:'Etiopía Yrgacheff', precio:9, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag4.png"},
+//     {pais:'Kenia Ndunduri', precio:15, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag5.png"},
+//     {pais:'Etiopía Sidamo', precio:17, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag6.png"},
+//     {pais:'Costa Rica Monte Bello', precio:12, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag7.png"},
+//     {pais:'Colombia La Casita', precio:9, url: "http://127.0.0.1:5500/proyectos-github/cafe-de-altura/assets/Coffee%20bag8.png"},
+// ];
 
-if (!JSON.parse(localStorage.getItem("data"))==null) {
-    let data = JSON.parse(localStorage.getItem("data")) || [];
-    console.log('si');
-}
-else{
-    buttons.forEach((e, i) => {
-        e.addEventListener('click', event => {
-            event.preventDefault()
-            let data = JSON.parse(localStorage.getItem("data")) || [];
-            let pais = document.querySelectorAll(".imgn")[i].children[1].children[0].innerHTML;
-            let precio = document.querySelectorAll(".imgn")[i].children[1].children[1].innerHTML;
-            let urlinicial = getComputedStyle(document.querySelectorAll(".imgn")[i].children[0]).getPropertyValue('background-image');
-            let url = urlinicial.slice(5, urlinicial.length - 2)
-            const objeto =
-            {
+// const changebags = (precio) => {
+//    return precio.toFixed(2).replace('.', ',')
+// }
+
+// const card2= document.querySelector('.cards2')
+// bags.forEach(bag =>{
+//     const div= document.createElement('div');
+//     div.classList='imgn flex'
+//     div.innerHTML= `<img src=${bag.url} alt="img bag">
+//                     <div class="price flex">
+//                         <a href="">${bag.pais}</a>
+//                         <p>${changebags(bag.precio)} €</p>
+//                     </div>
+//                     <button class="buttonAñadir">Añadir</button>
+//                     `;
+//                     card2.append(div)
+// });
+
+const buttons = document.querySelectorAll(".imgn button:last-child");
+console.log(JSON.parse(localStorage.getItem("data")) == null);
+
+
+
+buttons.forEach((e, i) => {
+    e.addEventListener('click', event => {
+        event.preventDefault();
+        const data = JSON.parse(localStorage.getItem("data")) || [];
+        let pais = `${document.querySelectorAll(".imgn")[i].children[1].children[0].innerHTML}`;
+        let precio = document.querySelectorAll(".imgn")[i].children[1].children[1].innerHTML;
+        let urlinicial = getComputedStyle(document.querySelectorAll(".imgn")[i].children[0]).getPropertyValue('background-image');
+        let url = urlinicial.slice(5, urlinicial.length - 2);
+
+        const repeated = data.find(bag => bag.pais === pais);
+        if (repeated) {
+            repeated.quantity += 1;
+            localStorage.setItem("data", JSON.stringify(data));
+        } else {
+            const bagdata = {
                 pais: pais,
                 precio: precio,
                 url: url,
-                quantity: 1
+                quantity: 1,
             }
-            console.log(objeto);
-            data.push(objeto)
-            localStorage.setItem("data", JSON.stringify(data))
+            data.push(bagdata);
+            localStorage.setItem("data", JSON.stringify(data));
+        };
+    });
+});
 
-        })
-    })
-}
-const savePokemon = (pais) => {
-    const data2 = JSON.parse(localStorage.getItem("data")) || [];
-    const repeated = data2.find(bag => bag.pais === pais);
-    console.log(repeated);
-    if (repeated) {
-        repeated.quantity += 1;
-        console.log(data2);
-        localStorage.setItem("data", JSON.stringify(data2));
-    } 
-    // else {
-    //     const pokeData = {
-    //         pais: pais,
-    //         name: name,
-    //         img: img,
-    //         quantity: 1
-    //     }
-    //     cart.push(pokeData);
-    //     localStorage.setItem("cart", JSON.stringify(cart));
-    // }
-}
-savePokemon(document.querySelectorAll(".imgn")[0].children[1].children[0].innerHTML)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // buttons.forEach((e, i) => {
@@ -71,8 +109,3 @@ savePokemon(document.querySelectorAll(".imgn")[0].children[1].children[0].innerH
 //         localStorage.setItem("data", JSON.stringify(data))
 //     })
 // })
-
-console.log(JSON.parse(localStorage.getItem('data')));
-
-let url = getComputedStyle(document.querySelectorAll(".imgn")[0].children[0]).getPropertyValue('background-image').slice(5, getComputedStyle(document.querySelectorAll(".imgn")[0].children[0]).getPropertyValue('background-image').length - 2);
-console.log(url);
